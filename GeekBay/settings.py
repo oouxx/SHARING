d2ebeaa820715ee7b2bf740acb923f54e225917f
oouxx <wangxinxing961129@gmail.com>
2018-12-23 18:43:59 +0800

    更新request和django,连接redis

--- GeekBay/settings.py
+++ GeekBay/settings.py
@@ -206,7 +206,7 @@ JWT_AUTH = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://:wxx1512@127.0.0.1:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
