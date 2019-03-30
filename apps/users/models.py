e51e9982569d14c88482c1e9fd1a0b4d031cccd6
oouxx <wangxinxing961129@gmail.com>
2019-03-31 06:51:01 +0800

    禁用后端自定义认证

--- apps/users/models.py
+++ apps/users/models.py
@@ -18,7 +18,7 @@ class UserProfile(AbstractUser):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-      return self.username
+        return self.username
 
 
 class VerifyCode(models.Model):
