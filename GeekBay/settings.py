74a3beba0c641f80c1f9398aa46ca9f66f5642aa
oouxx <wangxinxing961129@gmail.com>
2019-04-07 03:56:40 +0800

    fix some bugs

--- GeekBay/settings.py
+++ GeekBay/settings.py
@@ -133,9 +133,9 @@ USE_TZ = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
