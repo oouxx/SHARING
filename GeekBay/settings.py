a8e0622ddd6e3d9afe681b4467d2c38b48cd4b0b
oouxx <wangxinxing961129@gmail.com>
2019-04-07 02:31:22 +0800

    添加static 目录，删掉uwsgi.ini

--- GeekBay/settings.py
+++ GeekBay/settings.py
@@ -137,7 +137,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static")
 )
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
